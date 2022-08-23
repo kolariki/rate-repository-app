bd26b424e2d557db2e253c7c5ff78be485439f75
kolariki <ivankolariki1990@gmail.com>
2022-08-23 20:32:31 -0300

    react native

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import image from './assets/diamante2.png';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View styles={styles.container}>
+      <Text styles={styles.title}> DIAMONDS </Text>
+      <Image
+        source={image}
+      />
+    </View>);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: '#292929' },
+  title: { fontSize: 30, color: 'white' },
+  image: { height: 10, width: 10}
 });
+
+export default App;
