ca7164f38e5ecad6f00c757986db66a8f5fdc3f5
kolariki <ivankolariki1990@gmail.com>
2022-08-24 00:30:45 -0300

    cambios

--- App.js
+++ App.js
@@ -1,29 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
-import image  from './assets/diamante-negro.png';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing'
+
 
 const App = () => {
+
+  const [selectedImage, setSelectedImage] = useState(null)
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (permissionResult.granted === false) {
+      alert('permission to access camera is required');
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync()
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri })
+  }
+
+  const openShareDialog = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert('Sharing, is not available on your plataform');
+      return;
+    }
+await Sharing.shareAsync(selectedImage.localUri);
+  }
   return (
-  <View style={styles.container}>
-  <Text style={styles.title}> DIAMONDS </Text>
-  <Image 
-  source={image}
-  style ={styles.image}
-  />
-  <TouchableOpacity
-  onPress={() => Alert.alert('Hola!')}
-  style = {styles.button}>
-    <Text style = {styles.buttonText}> Press Me</Text>
-  </TouchableOpacity>
-  </View>);
+    <View style={styles.container}>
+      <Text style={styles.title}> Selecciona un Diamante </Text>
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+      >
+        <Image
+          source={{
+            uri: selectedImage !== null
+              ? selectedImage.localUri
+              : "https://picsum.photos/200/100"
+          }}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+      {
+        selectedImage ? (
+          <TouchableOpacity onPress={openShareDialog} style={styles.button}>
+        <Text style={styles.buttonText}> Comparte este diamante </Text>
+      </TouchableOpacity>
+        ) : (
+          <View/>
+        )}
+    </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black' },
-  title: { fontSize: 30, color:'white' },
-  image: {height: 300, width: 300},
-  button: {backgroundColor: 'blue', padding: 7, marginTop: 7},
-  buttonText: {color: '#fff', fontSize: 20}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black'
+  },
+  title: {
+    fontSize: 30,
+    color: 'white'
+  },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    resizeMode: 'contain',
+  },
+  button: {
+    backgroundColor: 'deepskyblue',
+    padding: 7,
+    marginTop: 7
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20
+  }
 });
 
 export default App;
